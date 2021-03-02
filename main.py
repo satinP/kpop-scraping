96e9cc53426fd76eb83b89b7bb829d28a1af2b27
Pedro Satin <pedro5satin@gmail.com>
2021-03-01 21:57:53 -0300

    Creating DataFrame from group page scraping

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://en.wikipedia.org'
-full_data = {}
+
+full_data = []
 
 
 def url_find(path='/w/index.php?title=Category:K-pop_music_groups'):
@@ -10,23 +12,29 @@ def url_find(path='/w/index.php?title=Category:K-pop_music_groups'):
 
     response = urlopen(full_url)
     html = response.read()
+    html = html.decode('utf-8')
     soup = BeautifulSoup(html, 'html.parser')
-
-    keys, values = [], []
 
     soup.find('div', {'class': 'mw-category-group'}).get_text()
     group_list = soup.find('div', {'class': 'mw-category'}).findAll("li")
 
     for gl in group_list:
-        keys.append(gl.get_text())
-        values.append(gl.find('a').get("href"))
+        group = {}
+        group['id'] = len(full_data)
+        group['name'] = gl.get_text()
+        group['relative_url'] = gl.find('a').get("href")
+        full_data.append(group)
 
-    full_data.update(dict(zip(keys, values)))
     href_pages = soup.find('a', {'title': 'Category:K-pop music groups'})
 
     if href_pages.get_text() == 'next page':
         url_find(href_pages.get("href"))
 
+    return pd.DataFrame(full_data)
 
-url_find()
-print(full_data)
+
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.width', None)
+# pd.set_option('display.max_colwidth', -1)
+pd = url_find()
