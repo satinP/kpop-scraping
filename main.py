cf95fde65cfe7db1f4dee24b5197ae435c5eb5a3
Pedro Satin <pedro5satin@gmail.com>
2021-03-01 21:58:50 -0300

    Function to get group main description

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ url = 'https://en.wikipedia.org'
 full_data = []
 
 
+def get_group_descripton(relative_url):
+    full_url = url + relative_url
+
+    response = urlopen(full_url)
+    html = response.read()
+    html = html.decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+    p_tags = soup.find('p')
+    for match in p_tags.findAll('sup'):
+        match.replaceWith('')
+
+    return p_tags.get_text()
+
+
 def url_find(path='/w/index.php?title=Category:K-pop_music_groups'):
     full_url = url + path
 
@@ -23,6 +37,7 @@ def url_find(path='/w/index.php?title=Category:K-pop_music_groups'):
         group['id'] = len(full_data)
         group['name'] = gl.get_text()
         group['relative_url'] = gl.find('a').get("href")
+        #group['description'] = get_group_descripton(group['relative_url'])
         full_data.append(group)
 
     href_pages = soup.find('a', {'title': 'Category:K-pop music groups'})
@@ -38,3 +53,6 @@ def url_find(path='/w/index.php?title=Category:K-pop_music_groups'):
 # pd.set_option('display.width', None)
 # pd.set_option('display.max_colwidth', -1)
 pd = url_find()
+
+# for index, row in pd.T.iteritems():
+#    get_group_descripton( row['relative_url'] )
